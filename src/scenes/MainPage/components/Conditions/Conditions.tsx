8f799cab325cc64ce564637fcdcb5e3f9c83d6f4
hulio-pc <ivetal820@gmail.com>
2021-10-15 15:20:16 +0300

    conditions

--- src/scenes/MainPage/components/Conditions/Conditions.tsx
+++ src/scenes/MainPage/components/Conditions/Conditions.tsx
@@ -1,56 +1,64 @@
 import React from "react";
 
+import './Conditions.scss';
 
 const Conditions = () => {
 
   return (
     <>
-      <div className="inform_block w100">
-        <div className="id" id="conditions"></div>
-        <div className="inform_blocks fx w">
+      <section id="conditions" className="section section-conditions">
+        <div className="container">
+          <div className="conditions__items">
+            <div className="conditions__item">
+              <img className='conditions__image' src="" alt=""/>
+              <h6 className="conditions__title">
+                Хто може подати заявку?
+              </h6>
+              <ul className="conditions__list">
+                <li>Фізічна особа, яка досягла 21 року</li>
+                <li>Юридична особа</li>
+              </ul>
+            </div>
 
-          <div className="item">
-            <i className="i-team"></i>
-            <p className="title"><span>Хто може подати заявку?</span></p>
-            <ul>
-              <li>Фізічна особа, яка досягла 21 року</li>
-              <li>Юридична особа</li>
-            </ul>
+            <div className="conditions__item">
+              <img className='conditions__image' src="" alt=""/>
+              <h6 className="conditions__title">
+                Вимоги до авто
+              </h6>
+              <ul className="conditions__list">
+                <li>Ваше авто не старше 2005 року</li>
+                <li>Ви є власником авто за тех паспортом</li>
+                <li>Авто на українській реєстрації</li>
+              </ul>
+            </div>
+
+            <div className="conditions__item">
+              <img className='conditions__image' src="" alt=""/>
+              <h6 className="conditions__title">
+                Умови договору
+              </h6>
+              <ul className="conditions__list">
+                <li>Кредит терміном від 1 місяця до 2 років</li>
+                <li>Ставка від 1% за програмою «на стоянку» і «в ходу»</li>
+                <li>Видаємо до 80% від вартості авто</li>
+                <li>Допустиме прострочення платежу - до 5 календарних днів без штрафних санкцій</li>
+              </ul>
+            </div>
+
+            <div className="conditions__item">
+              <img className='conditions__image' src="" alt=""/>
+              <h6 className="conditions__title">
+                Необхідні документи
+              </h6>
+              <ul className="conditions__list">
+                <li>Паспорт громадянина України</li>
+                <li>Свідоцтво про реєстрацію транспортного засобу</li>
+                <li>Довіреність на право розпорядження авто</li>
+              </ul>
+            </div>
           </div>
-
-          <div className="item">
-            <i className="i-car"></i>
-            <p className="title"><span>Вимоги до авто</span></p>
-            <ul>
-              <li>Ваше авто не старше 2005 року</li>
-              <li>Ви є власником авто за тех паспортом</li>
-              <li>Авто на українській реєстрації</li>
-            </ul>
-          </div>
-
-          <div className="item">
-            <i className="i-contract"></i>
-            <p className="title"><span>Умови договору</span></p>
-            <ul>
-              <li>Кредит терміном від 1 місяця до 2 років</li>
-              <li>Ставка від 1% за програмою «на стоянку» і «в ходу»</li>
-              <li>Видаємо до 80% від вартості авто</li>
-              <li>Допустиме прострочення платежу - до 5 календарних днів без штрафних санкцій</li>
-            </ul>
-          </div>
-
-          <div className="item">
-            <i className="i-document"></i>
-            <p className="title"><span>Необхідні документи</span></p>
-            <ul>
-              <li>Паспорт громадянина України</li>
-              <li>Свідоцтво про реєстрацію транспортного засобу</li>
-              <li>Довіреність на право розпорядження авто</li>
-            </ul>
-          </div>
-
         </div>
-      </div>
+      </section>
     </>
   );
 };
