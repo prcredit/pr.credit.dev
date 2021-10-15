736550e9edba0461200515cd0b8265761e9e12c4
hulio-pc <ivetal820@gmail.com>
2021-10-15 20:27:21 +0300

    build PROD

---
+++ build/precache-manifest.6851e3044954f46e956039f0e8ddeb97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d19a27fa9d53359860b2d52454f0b29",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0728a50bdf5532b634a9",
+    "url": "/static/css/main.56a63ed1.chunk.css"
+  },
+  {
+    "revision": "ff9f0cf603898ae0c62b",
+    "url": "/static/js/2.01a8fb85.chunk.js"
+  },
+  {
+    "revision": "48819809c9868bad9792add5948cce53",
+    "url": "/static/js/2.01a8fb85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0728a50bdf5532b634a9",
+    "url": "/static/js/main.58098e2c.chunk.js"
+  },
+  {
+    "revision": "80054c8b165bb1cd6a36",
+    "url": "/static/js/runtime-main.e8c570e1.js"
+  }
+]);
---
+++ src/scenes/MainPage/components/Directions/Directions.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import './Directions.scss';
+
+const Directions = (props: any) => {
+
+  return (
+    <section className='section '>
+      <div className="container">
+        <h1 className="title ">
+          PR CREDITS
+        </h1>
+
+
+
+      </div>
+    </section>
+  );
+};
+
+export default Directions;
