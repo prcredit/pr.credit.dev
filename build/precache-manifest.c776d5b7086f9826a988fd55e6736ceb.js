400636660c070cbf7bd7ea5c44e25a1554466f3a
hulio-pc <ivetal820@gmail.com>
2022-02-04 13:11:41 +0200

    build DEV

---
+++ build/precache-manifest.c776d5b7086f9826a988fd55e6736ceb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "412044b384b1faceacbd7b2c1a8dd238",
+    "url": "/index.html"
+  },
+  {
+    "revision": "787ba6add77b8bd19f1c",
+    "url": "/static/css/main.c1c7d289.chunk.css"
+  },
+  {
+    "revision": "bac6c2c7e81db87e4123",
+    "url": "/static/js/2.99a1fa7c.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.99a1fa7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "787ba6add77b8bd19f1c",
+    "url": "/static/js/main.045d9d99.chunk.js"
+  },
+  {
+    "revision": "032fc75a279aa53db0b9",
+    "url": "/static/js/runtime-main.adeadb3d.js"
+  }
+]);
